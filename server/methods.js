bccc1bb4612359db903ac0c9d2086389d46c2f25
Jonathan <blavier.jonathan@gmail.com>
2019-08-08 19:08:44 +0200

    separate methods and pubs

---
+++ server/methods.js
@@ -0,0 +1,14 @@
+import { Messages } from '../lib/collections/messages.js'
+
+Meteor.methods({
+    sendMessage: function (messageText) {
+      /* add authentication here */
+  
+      Messages.insert({
+        text: messageText,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username,
+      });
+    }
+  });
